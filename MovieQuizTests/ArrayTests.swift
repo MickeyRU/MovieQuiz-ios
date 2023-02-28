7542c61dc2b5db700d72e1f8f2be1bf8da7c7c1c
Pavel Afanasyev <afanasyevpv9@gmail.com>
2023-02-28 15:08:46 +0300

    UITest
    Написаны UITestы в соотвествии с материалами учебника

--- MovieQuizTests/ArrayTests.swift
+++ MovieQuizTests/ArrayTests.swift
@@ -24,14 +24,14 @@ class ArrayTests: XCTestCase {
     
     func testGetValueOutOfRange() throws { // тест на взятие элемента по неправильному индексу
         // Given
-            let array = [1, 1, 2, 3, 5]
-            
-            // When
-            let value = array[safe: 20]
-            
-            // Then
-            XCTAssertNil(value)
+        let array = [1, 1, 2, 3, 5]
+        
+        // When
+        let value = array[safe: 20]
+        
+        // Then
+        XCTAssertNil(value)
     }
 }
 
- 
+
